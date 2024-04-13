9199cd646edad7ca963745684ee2c3047e73b9fe
piotr <piotrwydmuch95@gmail.com>
2024-04-13 11:41:49 +0200

    vite setup: usePolling enabled for auto reload (HMR) fix

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,16 @@ import path from 'path';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  server: {
+    watch: {
+      /*  HotReload not working because I am using WSL2:  
+          https://vitejs.dev/config/server-options.html#server-watch 
+
+          Note that usePolling leads to high CPU utilization.
+      */
+      usePolling: true,
+    }
+  },
   resolve: {
     alias: {
       '@features': path.resolve(__dirname, './src/features'),
